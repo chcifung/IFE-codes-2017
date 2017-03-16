600726091d33d8165535d9ed212cca1fb20df5d2
chcifung <chcifung@gmail.com>
2017-03-16 19:42:09 +0800

    Add files via upload

--- scripts/practise103.js
+++ scripts/practise103.js
@@ -2,10 +2,26 @@
 //left-in
 function leftin(){
 	var left = document.getElementById("left-in");
+	var newlist = document.createElement("li");
+	var textNode = document.getElementById('number');
+	var list = document.getElementById("list");
 	left.onclick = function(){
-		document.getElementById('list').innerText=document.getElementById('number').value;;
+		newlist.appendChild(textNode);
+		list.insertBefore(newlist,list.childNodes[0]);
 	}
 
 }
 
+function rightin(){
+	var right = document.getElementById("right-in");
+	var newlist = document.createElement("li");
+	var textNode = document.getElementById('number');
+	var list = document.getElementById("list");
+	right.onclick = function(){
+		newlist.appendChild(textNode);
+		list.insertBefore(newlist,list.childNodes[list.length]);
+	}
+}
+
 leftin();
+rightin();
