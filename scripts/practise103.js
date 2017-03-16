ed3177d3bdfa3801d3dc3f5e3c30b947ae2ab9d1
chcifung <chcifung@gmail.com>
2017-03-16 19:24:54 +0800

    Add files via upload

--- scripts/practise103.js
+++ scripts/practise103.js
@@ -1,1 +1,11 @@
-document.ready();
+
+//left-in
+function leftin(){
+	var left = document.getElementById("left-in");
+	left.onclick = function(){
+		document.getElementById('list').innerText=document.getElementById('number').value;;
+	}
+
+}
+
+leftin();
