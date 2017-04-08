b0f1eec39827c7383b2e46f89c1999b13b2cfa5f
chcifung <chcifung@gmail.com>
2017-04-08 22:31:26 +0800

    Add files via upload

---
+++ scripts/practise29.js
@@ -0,0 +1,10 @@
+var number = document.getElementById("number");
+var letter = document.getElementById("letter");
+var test1 = document.getElementById("test1");
+var test2 = document.getElementById("test2");
+
+test1.onclick = function(){
+	var value1 = number.value;
+	console.log(value1);
+	
+}
