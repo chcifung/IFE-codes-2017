02eb9c62182bdea5a0bf7b5ac037d8c2ab7c03d2
chcifung <chcifung@gmail.com>
2017-04-09 22:58:51 +0800

    正则表达式之简易markdown文件解析器
    正则表达式之简易markdown文件解析器

--- scripts/practise30.js
+++ scripts/practise30.js
@@ -3,13 +3,15 @@ var submit = document.getElementById("submit");
 var right = document.getElementsByClassName("right")[0];
 var intent = intents.value;
 var re1 = /(\#){1}(\S*)/g;//h1
-		var re12 = /(\#){2}(\S*)/g;//h2
-		var re13 = /(\#){3}(\S*)/g;//h3
-		var re14 = /(\#){4}(\S*)/g;//h4
-		var re15 = /(\#){5}(\S*)/g;//h5
-		var re16 = /(\#){6}(\S*)/g;//h6
+var re12 = /(\#){2}(\S*)/g;//h2
+var re13 = /(\#){3}(\S*)/g;//h3
+var re14 = /(\#){4}(\S*)/g;//h4
+var re15 = /(\#){5}(\S*)/g;//h5
+var re16 = /(\#){6}(\S*)/g;//h6
 var re2 = /\!\[(.*?)\]\((.*?)\)/gi;//picture
 var re3 = /\[(.*?)\]\((.*?)\)/gi;//link
+var re4 = /(\*\s*)(\S*)/g;//list
+var re5 = /(\`){3}([\s\S]*?)(\`){3}/g;//code
 
 submit.onclick = function(){
 	right.innerHTML = intents.value.replace(re16,"<h6>$2</h6>");
@@ -18,6 +20,9 @@ submit.onclick = function(){
 	right.innerHTML = right.innerHTML.replace(re13,"<h3>$2</h3>");
 	right.innerHTML = right.innerHTML.replace(re12,"<h2>$2</h2>");
 	right.innerHTML = right.innerHTML.replace(re1,"<h1>$2</h1>");
+	right.innerHTML = right.innerHTML.replace(re4,"<li>$2</li>");
 	right.innerHTML = right.innerHTML.replace(re2,'<img src="$2" alt="$1">');
 	right.innerHTML = right.innerHTML.replace(re3,'<a href="$2">$1</a>');
+	
+	right.innerHTML = right.innerHTML.replace(re5,"<pre><code>$2</code></pre>");
 }
