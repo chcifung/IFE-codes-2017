63ba6762d4203c8df8fa7f5c5b257d071d449302
chcifung <chcifung@gmail.com>
2017-04-11 22:07:15 +0800

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,25 @@
-new Vue({
-
-	el: "#app",
-	data:{
-		message:"Helloword!"
+new Vue({ 
+	el: '#app', 
+	data: { 
+		book: { 
+			id: 0, 
+			author: '', 
+			name: '', 
+			price: '' 
+		}, 
+		books:	
+		[	
+			{ id: 1, author: '曹雪芹', name: '红楼梦', price: 32.0 }, 
+			{ id: 2, author: '施耐庵', name: '水浒传', price: 30.0 }, 
+			{ id: 3, author: '罗贯中', name: '三国演义', price: 24.0 }, 
+			{ id: 4, author: '吴承恩', name: '西游记', price: 20.0 }
+		] 
+	} ,
+	methods:{
+		addBook: function(){
+			this.book.id = this.books.length+1;
+			this.books.push(this.book);
+			this.book = '';
+		} 
 	}
-
-});
+})
