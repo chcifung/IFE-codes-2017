20acb4252e3380bbbaafecc8c0b3c5478970f46b
蒋怡 <chcifung@gmail.com>
2017-04-19 20:01:32 +0800

    Add files via upload

--- scripts/practise20.js
+++ scripts/practise20.js
@@ -1,11 +1,17 @@
- let app1 = new Observer({
-         name: 'youngwind',
-         age: 25
- });
-
- // 你需要实现 $watch 这个 API
- app1.$watch('age', function(age) {
-         console.log(`我的年纪变了，现在已经是：${age}岁了`)
- });
-
- app1.data.age = 100; // 输出：'我的年纪变了，现在已经是100岁了'
+ function Observer(data){
+ 	return new Proxy(data,{
+ 		get:function(target,key){
+ 			if(key in target){
+ 				console.log("you visit the " +key);
+ 				return target[key];
+ 			}else{
+ 				 throw new Error("you have the wrong visit");
+ 			}
+ 		},
+ 		set:function(target,key,newVal){
+ 			console.log("you set a new " +key);
+ 			console.log("new" + key + "=" +newVal);
+ 			target[key]=newVal;
+ 		}
+ 	})
+ }
