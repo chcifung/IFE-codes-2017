ea3d726da029b06244f64723df38b678cef376c4
蒋怡 <chcifung@gmail.com>
2017-04-19 20:36:42 +0800

    Add files via upload

--- scripts/practise21.js
+++ scripts/practise21.js
@@ -1,3 +1,37 @@
 function Observer(data){
 	this.data =data;
+	this.makeObserver(data);
+}
+
+Observer.prototype.setterAndGetter = function(key,val){
+	Object.defineProperty(this.data,key,{
+		enumerable:true,
+		configurable:true,
+		get:function(){
+			console.log('you visit the ' +key);
+			return val;
+		},
+		set:function(newVal){
+			if(typeof newVal ==='object'){
+				new Observer(val);
+			}
+			console.log("you set the new " +key);
+			console.log("the new " +key +"is" +newVal);
+			val = newVal;
+		}
+	})
+}
+
+Observer.prototype.makeObserver = function(obj){
+	let val;
+	for(key in obj){
+		if(obj.hasOwnProperty(key)){
+		val = obj[key];
+
+			if(typeof val === 'object'){
+				new Observer(val);
+			}
+		}
+			this.setterAndGetter(key,val);
+	}
 }
