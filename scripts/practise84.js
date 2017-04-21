17039f2c38c5c7c2c1504ce72ce9b595ca428d35
蒋怡 <chcifung@gmail.com>
2017-04-21 20:46:32 +0800

    Add files via upload

---
+++ scripts/practise84.js
@@ -0,0 +1,11 @@
+$(function () { 
+  $('#jstree_demo_div').jstree(); 
+  $('#jstree_demo_div').on("changed.jstree", function (e, data) {
+  console.log(data.selected);
+  });
+  $('button').on('click', function () {
+    $('#jstree').jstree(true).select_node('child_node_1');
+    $('#jstree').jstree('select_node', 'child_node_1');
+    $.jstree.reference('#jstree').select_node('child_node_1');
+});
+});
