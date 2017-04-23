178ef0e973fa10919e078d28722066943d058a5a
蒋怡 <chcifung@gmail.com>
2017-04-23 22:45:54 +0800

    Add files via upload

--- scripts/practise83.js
+++ scripts/practise83.js
@@ -5,15 +5,20 @@ var playpause = document.getElementById("playpause");
 function toogglePausePlay(){
 	if(audio.paused || audio.ended){
 		playpause.title = "pause";
-		playpause.innerHTML = "pause";
+		playpause.className = "glyphicon glyphicon-pause";
 		audio.play();
 	}else{
 		playpause.title = "play";
-		playpause.innerHTML = "play";
+		playpause.className = "glyphicon glyphicon-play";
 		audio.pause();
 	}
 }
 
 playpause.onclick = function(){
 	toogglePausePlay();
+}
+
+function setVolume(){
+	var volume = document.getElementById("volume");
+	audio.volume = volume.value;
 }
