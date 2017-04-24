16befea4e676566918041b32b67fa07d7bcfffac
蒋怡 <chcifung@gmail.com>
2017-04-24 19:45:23 +0800

    仿豆瓣FM音乐播放器

--- scripts/practise83.js
+++ scripts/practise83.js
@@ -16,9 +16,38 @@ function toogglePausePlay(){
 
 playpause.onclick = function(){
 	toogglePausePlay();
+	
 }
 
+var volume = document.getElementById("volume");
+
 function setVolume(){
-	var volume = document.getElementById("volume");
+
 	audio.volume = volume.value;
 }
+
+function toggleMute(){
+	if(audio.muted){
+		volume.className = "glyphicon glyphicon-volume-down";
+		audio.volume = volume.value;
+		audio.play();
+	}else{
+	audio.muted = !audio.muted;
+	volume.className = "glyphicon glyphicon-volume-off";
+	}
+}
+
+volume.onclick = function(){
+	toggleMute();
+}
+
+
+
+function updateProgress(){
+	var progress = document.getElementById("progress");
+	var value = 0;
+	value = Math.floor((100/audio.duration)*audio.currentTime);
+	progress.style.width = value +"%";
+}
+
+audio.addEventListener("timeupdate",updateProgress,false);
